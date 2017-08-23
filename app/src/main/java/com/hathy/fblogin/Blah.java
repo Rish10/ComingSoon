f7102174ab7b687a6f7c606f985643652dbf3360
Rishabh <rish0497@gmail.com>
2017-08-23 12:10:18 +0530

    Finally

---
+++ app/src/main/java/com/hathy/fblogin/Blah.java
@@ -0,0 +1,13 @@
+package com.hathy.fblogin;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Blah extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blah);
+    }
+}
